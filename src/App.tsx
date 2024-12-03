183c9342e3da1b426697a847955c4073e554494a
Marcano, Alejandro (Externo - WITI) <alejandro.marcano@externos-cl.cencosud.com>
2024-12-03 16:14:20 -0400

    clean proejct and push sala and category sala info

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Loader from "./common/Loader";
+import CategoriasSalasAdminPage from "./pages/CategoriaSalas/sala-admin.page";
+import SalasAdminPage from "./pages/Salas/sala-admin.page";
+import DefaultLayout from "./components/layout/DefaultLayout";
+import PageTitle from "./components/PageTitle";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <DefaultLayout>
+      <Routes>
+        <Route
+          path="/sala"
+          element={
+            <>
+              <PageTitle title="Salas del Cine | CineAdmin" />
+              <SalasAdminPage />
+            </>
+          }
+        />
+
+        <Route
+          path="/categoria-sala"
+          element={
+            <>
+              <PageTitle title="Categorias de las Salas del Cine | CineAdmin" />
+              <CategoriasSalasAdminPage />
+            </>
+          }
+        />
+      </Routes>
+    </DefaultLayout>
   );
 }
 
